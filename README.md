a11307857b1c6fdef96b44096739a13791715c9a
vlbthambawita <vlbthambawita@gmail.com>
2021-12-18 09:41:46 +0000

    updated readme

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # singan-seg-polyp
 
-## If you want to train your own SinGAN-Seg models, please follow the repository here: https://github.com/vlbthambawita/singan-seg
+## If you want to train your own SinGAN-Seg models, please follow the repository here: https://github.com/vlbthambawita/singan-seg-polyp
 
 ## Install pip package
 ```shell
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="singan-seg-polyp", # Replace with your own username
-    version="1.0.5",
+    version="1.0.6",
     author="Vajira Thambawita",
     author_email="vlbthambawita@gmail.com",
     description="Generating synthetic polyps and corresponding mask using pretrained SinGAN-Seg and Style tranfering functionalities.",
